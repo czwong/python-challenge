8a296f9324888bbd119f5d0da51be312c21c4326
Chris Wong <chriswong90012@yahoo.com>
2019-02-01 14:59:49 -0800

    Added PyParagraph

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -12,25 +12,32 @@ with open(txtpath,'r') as txtfile:
 
     word_count=0
     sentence_count=0
+    letter_count=0
     word = []
     sentence = []
-    print(repr(txtreader))
-    for i in re.split("[,-.!?\s]",txtreader):
-        word_count+=1
-        # print(i)
-        word.append(i)
-    
-    for i in re.split("([.!?]?<=) +",txtreader):
+
+    for sentences in re.split("(?<=[.!?]) +",txtreader):
         sentence_count+=1
-        sentence.append(i)
+        sentence.append(sentences)
 
-    letters=[]
-    for i in word:
-        letters.append(re.split("[,]",i))
-    
+        for words in re.findall("[\w']+",sentences):
+            word_count+=1
+            word.append(words)
+            
+            for letters in re.findall(".",words):
+                # print(letters)
+                letter_count+=1
 
-    print(word_count)
-    print(sentence_count)
-    # print(sentence)
+    sentence_avg = round(word_count/sentence_count,2)
 
+    print("\nParagraph Analysis\n-----------------")
+    print(f"Approximate Word Count: {word_count}")
+    print(f"Approximate Sentence Count: {sentence_count}")
+    print(f"Average Sentence Length: {sentence_avg}")
+    # print(word_count/len(word))
 
+para = open("PyParagraph.txt","w")
+para.write("Paragraph Analysis\n-----------------\n")
+para.write(f"Approximate Word Count: {word_count}\n")
+para.write(f"Approximate Sentence Count: {sentence_count}\n")
+para.write(f"Average Sentence Length: {sentence_avg}")
