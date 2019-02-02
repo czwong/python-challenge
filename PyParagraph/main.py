603c4110332b2a732bdbec338c3ab00f5e87d466
Chris Wong <chriswong90012@yahoo.com>
2019-02-01 21:32:02 -0800

    Updated PyParagraph/Finished Assignment

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -20,24 +20,30 @@ with open(txtpath,'r') as txtfile:
         sentence_count+=1
         sentence.append(sentences)
 
+        #Matches only words and words with apostrophe
         for words in re.findall("[\w']+",sentences):
             word_count+=1
             word.append(words)
             
+            #Breaks words into letters
             for letters in re.findall(".",words):
-                # print(letters)
-                letter_count+=1
+
+                #Counts only if letters are alpha (excludes apostrophe)
+                if letters.isalnum():
+                    letter_count+=1
 
     sentence_avg = round(word_count/sentence_count,2)
+    letter_avg = round(letter_count/len(word),2)
 
     print("\nParagraph Analysis\n-----------------")
     print(f"Approximate Word Count: {word_count}")
     print(f"Approximate Sentence Count: {sentence_count}")
+    print(f"Average Letter Count: {letter_avg}")
     print(f"Average Sentence Length: {sentence_avg}")
-    # print(word_count/len(word))
 
 para = open("PyParagraph.txt","w")
 para.write("Paragraph Analysis\n-----------------\n")
 para.write(f"Approximate Word Count: {word_count}\n")
 para.write(f"Approximate Sentence Count: {sentence_count}\n")
+para.write(f"Average Letter Count: {letter_avg}\n")
 para.write(f"Average Sentence Length: {sentence_avg}")
